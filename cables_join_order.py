4593e54792299a93de6438413e351fe7cc783a4f
Roman Serafymovych <serafymovychr@gmail.com>
2024-03-06 23:54:29 +0300

    first release

---
+++ cables_join_order.py
@@ -0,0 +1,26 @@
+import heapq
+
+def cable_join_order(iterable):
+    # Створюємо мінімальну купу з усіх елементів ітерабельного об'єкта.
+    h = []
+    i = 1
+    for value in iterable:
+        heapq.heappush(h, value)
+    
+    # Витягуємо елементи впорядковано, формуючи відсортований масив.
+    while len(h) > 1:
+        join_1 = heapq.heappop(h)
+        join_2 = heapq.heappop(h)
+
+        print(f'Joint {i}: {join_1} and {join_2}')
+
+        sum = join_1 + join_2
+
+        heapq.heappush(h, sum)
+
+        i += 1
+
+# Масив для сортування
+arr = [12, 11, 13, 5, 6, 7]
+cable_join_order(arr)
+
